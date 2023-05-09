0c9e0d9f9fb0237a61ca9ca3fb82f86b4eb39a40
Vishva <vishva090300@gmail.com>
2023-05-09 13:59:13 +0530

    bug cleared in customer api

--- CRM-server/controllers/customerController.js
+++ CRM-server/controllers/customerController.js
@@ -13,7 +13,7 @@ module.exports = {
       Website: req.body.Website,
       Assigned: req.body.Assigned,
       Teams: req.body.Teams,
-      Partner: req.body.Partner,  
+      Partner: req.body.Partner,
       Category: req.body.Category,
       Department: req.body.Department,
       BusinessRole: req.body.BusinessRole,
@@ -34,8 +34,16 @@ module.exports = {
 
     try {
       const dataToSave = await data.save();
-      res.status(200).json(dataToSave);
-      console.log("Details added");
+      if (!dataToSave) {
+        console.log("Details failed");
+        res.status(400).json({ message: "fail to save" })
+      }
+      else {
+        res.status(200).json(dataToSave);
+        console.log("Details added");
+      }
+
+
     }
     catch (error) {
       res.status(400).json({ message: error.message })
