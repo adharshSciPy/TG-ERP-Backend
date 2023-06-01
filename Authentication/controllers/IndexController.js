c82b6b531efc03fdb2f8edb472f28a26b271969d
SOORAJ <soorajh.scipy@gmail.com>
2023-06-01 14:36:41 +0530

    indexing completed

--- Authentication/controllers/IndexController.js
+++ Authentication/controllers/IndexController.js
@@ -4,7 +4,17 @@ module.exports = {
     addIndex: async (req, res) => {
         const data = new Index({
           CompanyID : req.body.CompanyID,
-          CrmID:req.body.CrmID
+          CrmID:req.body.CrmID,
+          EmployeeID :req.body.EmployeeID,
+          InventoryID: req.body.Inventory,
+          PRJID: req.body.PRJ,
+          ProductID: req.body.ProductID,
+          PurchaseID: req.body.PurchaseID,
+          PurchaseorderID: req.body.PurchaseorderID,
+          PurchaseitemID:req.body.PurchaseitemID,
+          RFQID: req.body.RFQID,
+          InvoiceID: req.body.InvoiceID,
+          SalesID: req.body.SalesID,
         });
        
         console.log(data);
