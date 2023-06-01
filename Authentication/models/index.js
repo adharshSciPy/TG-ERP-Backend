b7ced8ace4614a5d6a9815a6fa6a1060d5d356c3
Monisha A <monishaa7000@gmail.com>
2023-06-01 11:51:08 +0530

    Authentication-models-index.js

--- Authentication/models/index.js
+++ Authentication/models/index.js
@@ -7,7 +7,47 @@ const indexSchema = new mongoose.Schema({
     CrmID: {
         type : String,
         unique : true
-    }   
+    },
+    EmployeeID: {
+        type : String,
+        unique : true
+    }, 
+    InventoryID: {
+        type : String,
+        unique : true
+    }, 
+    PRJID: {
+        type : String,
+        unique : true
+    },   
+    ProductID: {
+        type : String,
+        unique : true
+    },   
+    PurchaseID: {
+        type : String,
+        unique : true
+    },  
+    PurchaseorderID: {
+        type : String,
+        unique : true
+    }, 
+    PurchaseitemID: {
+        type : String,
+        unique : true
+    },  
+    RFQID: {
+        type : String,
+        unique : true
+    }, 
+    InvoiceID: {
+        type : String,
+        unique : true
+    },   
+    SalesID: {
+        type : String,
+        unique : true
+    }    
 })
 const Index = mongoose.model("index", indexSchema);
 module.exports = Index;
